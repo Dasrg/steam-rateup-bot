dc5015c38f0240c10f1a963f34a18b8b2e2c146b
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2023-04-24 13:03:04 +0200

    Revised the code
    - Use better text formatting
    - Replaced all `var` with `const` (You should'nt use vars, they are not safe)
    - Removed comments (There were a little too few anyways, and I think good programmer will know what each part does)
    - Shortened many parts of the code
    - Used guard clauses (You dont need to write `else` everywhere :)
    - Replaced config.json with config.js for better readability and loading it in index

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	id: "ID",
+	perChunk: 1,
+	betweenChunks: 5000,
+	amount: 0,
+	likes: true,
+	favorites: true,
+	appid: "APPID", //730 - CS:GO Guides, 767 - CS:GO Artworks, 440 - TF2, 520 - Dota 2
+	limited: false
+}
